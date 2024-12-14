e4dfca4757a83869042b3aa48a5935672aa8c1c0
Luu Minh Thien <128106955+th1enlm02@users.noreply.github.com>
2024-12-14 23:42:45 +0700

    stable config

--- scripts/haproxy_setup.sh
+++ scripts/haproxy_setup.sh
@@ -8,7 +8,7 @@ sudo apt install net-tools -y
 sudo curl https://dl.min.io/client/mc/release/linux-amd64/mc \
   -o /usr/local/bin/mc
 sudo chmod +x /usr/local/bin/mc
-sudo mc alias set minio http://10.0.30.10:9000 ogXBMfE3Xi5Oypz9G2Bo nMgyxNjc3pGMICtGld9WK7EdEpE6AgQ9hu0XxtCw
+sudo mc alias set minio http://10.0.30.10:9000 hibP9Mak4QpoO5TyeE0W rh1YyrtU6Yv7MbwWw99Te3j5SHbOFnew3s3dzzpf
 sudo mkdir -p /etc/haproxy/certs
 sudo mc cp minio/haproxy-config/th1enlm02.live.pem /etc/haproxy/certs/th1enlm02.live.pem
 sudo chmod 600 /etc/haproxy/certs/th1enlm02.live.pem
